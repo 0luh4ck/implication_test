8df996e2443bb5b181ff378329a92fceae0b4d9b
0luh4ck <email-0luh4ck@example.com>
2026-02-16 17:24:47 +0100

    fix: corrected the .env and the supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,8 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // Configuration Supabase
 // IMPORTANT: Remplacez ces valeurs par vos propres clés Supabase
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY  || 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://celsvptdaaswvhnnlzbs.supabase.co';
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY  || 'sb_publishable_KdcOwpKfU6efDYdGT5hBgQ_7LNFFRfY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
